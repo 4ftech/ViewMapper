8c1ae719dc799f6b980431599e69f646f5101a33
Nick Kuyakanon <nickuya@gmail.com>
2017-03-02 17:34:10 -0600

    better generic eureka mapping

--- EurekaViewMapper/EurekaViewMapper.swift
+++ EurekaViewMapper/EurekaViewMapper.swift
@@ -10,29 +10,54 @@ import Foundation
 import ViewMapper
 import Eureka
 
-public func <- (left: TextRow, right: String?) -> TextRow {
+public func <- <T, U> (left: T, right: U) -> T where T:RowType, T:BaseRow, T:TypedRowType, T.Cell.Value == U {
   left.value = right
   return left
 }
 
-public func <- (left: TextRow, right: UnsafeMutablePointer<String?>) -> TextRow {
-  left.onChange() { textRow in
-    right.pointee = textRow.value
+public func <- <T, U> (left: T, right: UnsafeMutablePointer<U?>) -> T where T:RowType, T:BaseRow, T:TypedRowType, T.Cell.Value == U {
+  left.onChange() { row in
+    right.pointee = row.value
   }
   left.value = right.pointee
-  
   return left
 }
 
-public func <- (left: TextRow, right: UnsafeMutablePointer<String>) -> TextRow {
-  left.onChange() { textRow in
-    if let value = textRow.value {
+public func <- <T, U> (left: T, right: UnsafeMutablePointer<U>) -> T where T:RowType, T:BaseRow, T:TypedRowType, T.Cell.Value == U {
+  left.onChange() { row in
+    if let value = row.value {
       right.pointee = value
-    } else {
-      right.pointee = ""
     }
   }
   left.value = right.pointee
   
   return left
 }
+
+//
+//public func <- (left: TextRow, right: String?) -> TextRow {
+//  left.value = right
+//  return left
+//}
+//
+//public func <- (left: TextRow, right: UnsafeMutablePointer<String?>) -> TextRow {
+//  left.onChange() { textRow in
+//    right.pointee = textRow.value
+//  }
+//  left.value = right.pointee
+//  
+//  return left
+//}
+//
+//public func <- (left: TextRow, right: UnsafeMutablePointer<String>) -> TextRow {
+//  left.onChange() { textRow in
+//    if let value = textRow.value {
+//      right.pointee = value
+//    } else {
+//      right.pointee = ""
+//    }
+//  }
+//  left.value = right.pointee
+//  
+//  return left
+//}
