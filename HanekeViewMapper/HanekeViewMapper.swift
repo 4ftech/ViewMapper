78541b9bd0800416b6d07d6cc4364039a2339164
Nick Kuyakanon <nickuya@gmail.com>
2017-05-01 11:30:28 -0500

    carthage update

--- HanekeViewMapper/HanekeViewMapper.swift
+++ HanekeViewMapper/HanekeViewMapper.swift
@@ -3,7 +3,7 @@
 //  ViewMapper
 //
 //  Created by Nick Kuyakanon on 2/22/17.
-//  Copyright © 2017 Oinkist. All rights reserved.
+//  Copyright © 2017 4f Tech. All rights reserved.
 //
 
 
--- ViewMapper/ViewMappable.swift
+++ ViewMapper/ViewMappable.swift
@@ -3,7 +3,7 @@
 //  ViewMappable
 //
 //  Created by Nick Kuyakanon on 2/21/17.
-//  Copyright © 2017 Oinkist. All rights reserved.
+//  Copyright © 2017 4f Tech. All rights reserved.
 //
 
 import Foundation
